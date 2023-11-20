0fff20c51c6bd26cad37b17f4acf631eaeb573be
Walter Cueva <cuevawalter@gmail.com>
2023-11-19 23:23:51 -0800

    Cange Verbage

--- functions.js
+++ functions.js
@@ -14,7 +14,7 @@ newsLetterButton.addEventListener('click', () => {
     findAlert.parentElement.removeChild(findAlert);
   }
   if (!newsLetterEmail.value) {
-    newParagraph.textContent = 'Please enter something!';
+    newParagraph.textContent = 'Please enter a value!';
     newParagraph.classList.add('alert-danger', 'w-100', 'p-2');
     newParagraph.setAttribute('id', 'alert');
     newsLetterSection.appendChild(newParagraph);
